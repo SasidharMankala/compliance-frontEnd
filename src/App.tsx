d3066c4a6e86c65befedcb1f1c6bba3882c32587
Sasidhar Mankala <60248183+SasidharMankala@users.noreply.github.com>
2025-08-29 04:03:21 -0400

    vercel

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white px-4 py-24 dark:bg-gray-900">
       <NavigationBar />
+      <p className="text-gray-600 dark:text-gray-100 font-black text-2xl" >Enter the details on the left to get started and know about your business compliance requirements.</p>
       <HeroSection />
     </main>
   );
